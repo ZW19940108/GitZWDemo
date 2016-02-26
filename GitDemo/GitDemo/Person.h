397abf8041c099132d38aab1458da711bc31f0ab
zhangwei <976667870@qq.com>
2016-02-26 14:20:04 +0800

    create Person work1

---
+++ GitDemo/GitDemo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  GitDemo
+//
+//  Created by qianfeng007 on 16/2/26.
+//  Copyright © 2016年 张卫. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ GitDemo/GitDemo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  GitDemo
+//
+//  Created by qianfeng007 on 16/2/26.
+//  Copyright © 2016年 张卫. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
